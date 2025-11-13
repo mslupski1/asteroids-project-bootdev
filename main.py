046ee2cad9ce4c360ee75769e4814ee4d680565d
mslupski1 <mslupski1@gmail.com>
2025-11-13 18:39:03 +0100

    Adding player sprite

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
-
-
+from circleshape import CircleShape
+from player import Player
 
 
 
@@ -14,12 +14,14 @@ def main():
     Clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    spaceship = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        spaceship.draw(screen)
         pygame.display.flip()
         Clock.tick(60)
         dt = Clock.get_time() / 1000
