79feefb3b59336ecc903f05e88039f4bf6681fdd
mslupski1 <mslupski1@gmail.com>
2025-11-13 18:12:00 +0100

    Adding clock and dt variable

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
 
 
 
@@ -11,6 +10,10 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    Clock = pygame.time.Clock()
+    dt = 0
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         log_state()
         for event in pygame.event.get():
@@ -18,6 +21,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        Clock.tick(60)
+        dt = Clock.get_time() / 1000
 
 
 if __name__ == "__main__":
