9e640a451f36d0823eae6e730dd3b59e521e7235
mslupski1 <mslupski1@gmail.com>
2025-11-16 18:31:26 +0100

    Added asteroids, asteroid field, collision

---
+++ asteroid.py
@@ -0,0 +1,17 @@
+import pygame
+from circleshape import CircleShape
+from constants import LINE_WIDTH
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+
+# python
+    def draw(self, screen):
+        center = (int(self.position.x), int(self.position.y))
+        pygame.draw.circle(screen, "white", center, int(self.radius), LINE_WIDTH)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
+
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pygame
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-from logger import log_state
+import sys
+from constants import *
+from logger import log_state, log_event
 from circleshape import CircleShape
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 
@@ -16,20 +19,29 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    field = AsteroidField()
     spaceship = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        dt = Clock.tick(60) / 1000
         screen.fill("black")
         updatable.update(dt)
-        for i in drawable:
-            i.draw(screen)
+        for a in asteroids:
+            if a.collides_with(spaceship):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+        for spr in drawable:
+            spr.draw(screen)
         pygame.display.flip()
-        Clock.tick(60)
-        dt = Clock.get_time() / 1000
 
 
 if __name__ == "__main__":
